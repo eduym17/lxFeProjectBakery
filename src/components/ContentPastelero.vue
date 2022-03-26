462a863a523fe5f8a6acef6fa36852c860bb48ff
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-03-25 20:53:48 -0600

    feat: pastelero page

--- src/components/ContentPastelero.vue
+++ src/components/ContentPastelero.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="content">
     <h1>¡Sobran muchos pasteles aún!</h1>
+    <h2>Pide el tuyo aquí:</h2>
+    <router-link to="/pedidos" class="pedidos">
+      <button>¡Quiero pastelillos!</button>
+    </router-link>
   </div>
 </template>
 
@@ -18,18 +22,28 @@ export default {
     background-repeat: no-repeat;
     background-position: center;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     gap: 25px;
     overflow: hidden;
   }
 
-  h1 {
+  h1, h2, button {
     color: white;
     background: #6D28D9;
     padding: 10px;
-    margin: 25px;
+    margin: 5px 25px;
     border-radius: 15px;
+    border: none;
   }
 
+  button {
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background: #5519b6;
+  }
 </style>
